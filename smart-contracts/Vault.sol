9b6c17a47b87af17e52beb2cfb6b1a48aefcd85e
ghostblue058 <265764363+ghostblue058@users.noreply.github.com>
2025-11-20 19:32:43 +0700

    test: add unit tests for endpoints in Vault.sol

--- smart-contracts/Vault.sol
+++ smart-contracts/Vault.sol
@@ -49,3 +49,4 @@ contract EnterpriseYieldVault is ReentrancyGuard, Ownable {
 // Optimized logic batch 6426
 // Optimized logic batch 2928
 // Optimized logic batch 2456
+// Optimized logic batch 1891
